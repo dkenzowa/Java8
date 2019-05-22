dc0b324e682f3a7ef9d738dcd7f7b72c42ba8efb
Danilo Kenzo <dani.kenzo@hotmail.com>
2019-05-22 13:25:39 -0300

    Adicionando lambda

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -6,52 +6,60 @@ import java.util.function.Consumer;
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		//Antes java 8
+		// Antes java 8
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("Alura");
 		palavras.add("Udemy");
 		palavras.add("Casa do código");
 		palavras.add("Caelum");
-		
+
 		Comparator<String> comparador = new ComparadorPorTamanho();
-		//Collections.sort(palavras, comparador);
-		
-		//Java 8
-		palavras.sort(comparador);
-		
+		// Collections.sort(palavras, comparador);
+
+		// Java 8
+//		palavras.sort((s1, s2) -> {
+//			if (s1.length() < s2.length()) {
+//				return -1;
+//			}
+//			if (s1.length() > s2.length()) {
+//				return 1;
+//			}
+//			return 0;
+//		});
+
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(palavras);
-		
-		//Antes Java 8
+
+		// Antes Java 8
 		for (String p : palavras) {
 			System.out.println(p);
 		}
-		
-		//Java 8
-		Consumer<String> consumidor = new ImprimeraNaLinha();
-		palavras.forEach(consumidor);
+
+		// Java 8
+//		Consumer<String> consumidor = new Consumer<String>() {
+//
+//			@Override
+//			public void accept(String t) {
+//				System.out.println(t);
+//			}
+//		};
+
+		palavras.forEach(t -> System.out.println(t));
 	}
 }
 
-class ImprimeraNaLinha implements Consumer<String>{
-
-	@Override
-	public void accept(String t) {
-		System.out.println(t);
-	}
-	
-}
-
-class ComparadorPorTamanho implements Comparator<String>{
+class ComparadorPorTamanho implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length()) {
+		if (s1.length() < s2.length()) {
 			return -1;
 		}
-		if(s1.length() > s2.length()) {
+		if (s1.length() > s2.length()) {
 			return 1;
 		}
 		return 0;
 	}
-	
+
 }
